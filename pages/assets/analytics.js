7f388970cf326efff48078e393101b97053a7609
Imran Remtulla <contact@imranr.dev>
2026-06-24 09:20:34 +0100

    Update Plausible script

--- pages/assets/analytics.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement("script");
-script.defer = true;
-script.setAttribute("data-domain", "wiki.obtainium.imranr.dev");
-script.src = "https://plausible.imranr.dev/js/script.outbound-links.js";
-document.head.appendChild(script);
